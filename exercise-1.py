625b3517edb9f076e56b70171eb1f3679d6010af
Chris Mędrela <chris.medrela+github@gmail.com>
2018-05-18 22:22:45 +0200

    Update

---
+++ exercise-1.py
@@ -0,0 +1,35 @@
+# encoding: utf-8
+
+"""
+W Pythonie funkcje są first-class citizens. Oznacza to, że argumentami do
+funkcji mogą być nie tylko dane (liczby, napisy, listy itd.), ale także inne
+funkcje. Tak samo funkcje mogą zwracać funkcje.
+
+Do czego to się przydaje? Klasyczny przykład to wbudowana funkcja filter.
+Pozwala ona na wyrzucenie z kolekcji elementów, które nie spełniają jakiegoś
+warunku. Warunkiem jest funkcja, która przyjmuje pojedynczy element i zwraca
+True lub False, w zależności od tego, czy dany element należy zostawić, czy go
+wyrzucić.
+"""
+
+numbers = [1, 2, 3, 4, 5, 6]
+def is_even(n):
+    return n % 2 == 0
+
+filtered = filter(is_even, numbers)
+print(list(filtered))  # ==> [2, 4, 6]
+
+"""
+Jako ćwiczenie, spróbuj wykorzystać podobną funkcję - wbudowaną funkcję map.
+Przekształca ona każdy element jakiejś kolekcji w dowolny sposób. Pierwszym
+argumentem jest funkcja, która przyjmuje element i powinna zwrócić
+przekształcony element (np. pomnożony przez dwa). Drugim argumentem jest
+kolekcja, której elementy chcemy przekształcić. Spróbuj przy pomocy tej funkcji
+usunąć początkowe i końcowe białe znaki (spacje) z listy stringów (zmienna
+`text`). Hint: metoda .strip()
+"""
+
+text = ['   tekst', 'z niepotrzebnymi    ', '  spacjami  ']
+...
+mapped = ...
+print(list(mapped))  # ==> ['tekst', 'z niepotrzebnymi', 'spacjami']
---
+++ exercise-2.py
@@ -0,0 +1,39 @@
+# encoding: utf-8
+
+"""
+Python pozwala na zagnieżdżanie funkcji. Przedstawiono to na przykładzie funkcji
+new_counter. Funkcja inc ma dostęp do wszystkich zmiennych lokalnych
+zdefiniowanych w new_counter - jest to tzw. domknięcie (closure). Jeśli jednak
+chcemy je nadpisać, konieczne jest użycie `nonlocal counter`, w przeciwnym razie
+Python zakłada, że chodzi o zmienną lokalną dla inc. Zwróć też uwagę, że funkcja
+new_counter zwraca funkcję inc.
+"""
+
+def new_counter():
+    counter = 0
+    def inc():
+        nonlocal counter
+        counter += 1
+        return counter
+    return inc
+
+counter = new_counter()
+counter_2 = new_counter()
+print(counter())  # ==> 1
+print(counter())  # ==> 2
+print(counter_2())  # ==> 1
+print(counter())  # ==> 3
+
+"""
+Jako ćwiczenie, spróbuj zaimplementować funkcję generate_adder(a), która zwraca
+funkcję zagnieżdżoną adder(b) zwracającą sumę liczb a i b. 
+"""
+
+def generate_adder(a):
+    ...
+
+add_5 = generate_adder(5)
+add_10 = generate_adder(10)
+print(add_5(7))  # ==> 12
+print(add_5(15))  # ==> 20
+print(add_10(33))  # ==> 43
